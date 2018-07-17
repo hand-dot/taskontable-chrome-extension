2a2ea21fb3550ea7afff8ad2fc309ef16093e810
hand-dot <kyouhei.fukuda0729@gmail.com>
2018-07-17 14:10:47 +0900

    add install event

--- background.js
+++ background.js
@@ -1,17 +1,15 @@
 var windowObjectReference = null;
 
 function openWindow(){
-  if(windowObjectReference == null || windowObjectReference.closed){
+  if(windowObjectReference === null || windowObjectReference.closed){
     windowObjectReference = window.open(navigator.onLine ? 'https://taskontable.com/' : 'offline.html' , 'Taskontable', 'resizable,scrollbars,status');
   }else{
     windowObjectReference.focus();
   };
 }
 
-chrome.browserAction.onClicked.addListener(function() {
-  openWindow();
-});
+chrome.runtime.onInstalled.addListener(openWindow);
 
-chrome.runtime.onStartup.addListener(function() {
-  openWindow();
-});
+chrome.browserAction.onClicked.addListener(openWindow);
+
+chrome.runtime.onStartup.addListener(openWindow);
